24769edf8e492ea14abf853911e24a57974b97f1
AMO15310 <wachiraamos402@gmail.com>
2023-10-08 19:31:42 +0300

    feat:login and signup pages

---
+++ src/app/(pages)/signup/page.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+const SignUpPage: React.FC = () => {
+  return (
+    <div className="h-full">
+      <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+        <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+          <Image
+            src="/draw2.webp"
+            priority
+            className="w-[80%] pt-[0%]"
+            alt="Sample image"
+            width={250}
+            height={250}
+          />
+        </div>
+
+        <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+          <form>
+            <section className="h-screen bg-inherit flex items-center justify-center">
+              <div className="rounded-lg p-8 shadow-md w-96">
+                <h2 className="text-2xl font-bold mb-4">Str3am Sign Up</h2>
+
+                <button className="flex items-center space-x-2 bg-green-500 text-white rounded-lg p-2 w-full mb-4 hover:bg-green-600">
+                  <img
+                    src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                    className="w-full"
+                    alt="Sample image"
+                  />
+                  <span>Sign Up with Spotify</span>
+                </button>
+
+                <button className="flex items-center space-x-2 bg-red-500 text-white rounded-lg p-2 w-full hover:bg-red-600">
+                  <Image
+                    src="/google.svg"
+                    alt="spotify"
+                    width={25}
+                    height={25}
+                  />
+                  <span>Sign Up with Google</span>
+                </button>
+
+                <div className="text-center text-gray-600 my-4">Or</div>
+
+                <form>
+                  <div className="mb-4">
+                    <label className="block text-gray-600 font-medium">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-900"
+                      placeholder="Your email"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-gray-600 font-medium">
+                      Username
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500 text-gray-900"
+                      placeholder="Your username"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label className="block text-gray-600 font-medium">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      className="text-gray-900 w-full border border-gray-300 rounded-lg py-2 px-3 focus:outline-none focus:border-blue-500"
+                      placeholder="Your password"
+                    />
+                  </div>
+
+                  <div className="mb-6 flex items-center">
+                    <input type="checkbox" className="mr-2" id="agreeTerms" />
+                    <label htmlFor="agreeTerms" className="text-gray-600">
+                      I agree to the Terms of Service
+                    </label>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="w-full bg-blue-500 text-white rounded-lg py-2 hover:bg-blue-600"
+                  >
+                    Sign Up
+                  </button>
+                </form>
+
+                <p className="mt-4 text-center text-gray-600">
+                  Already have an account?{" "}
+                  <Link href="/login">
+                    <p className="text-blue-500">Login</p>
+                  </Link>
+                </p>
+              </div>
+            </section>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
